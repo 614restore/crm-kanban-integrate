e6e292822ed4c35fec8a999e3d3ee8a24febd5ff
Jeff <scopemgr.app@gmail.com>
2026-02-21 23:18:43 -0500

    Add GH Pages deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const base = mode === "production" ? "/crm-kanban-integrate/" : "/";
+
+  return {
+    base,
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [react()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
